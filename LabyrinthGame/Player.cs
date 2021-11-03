febe305dde358b0c07a82d7c8b0a33921ebbd3bf
kiss-mate <kiss.mate.deik@gmail.com>
2021-11-03 20:37:05 +0100

    created Player class

---
+++ LabyrinthGame/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabyrinthGame
+{
+    internal class Player
+    {
+        public Position Position { get; set; }
+        public int Health { get; set; }
+        public char Avatar { get; set; }
+    }
+}
