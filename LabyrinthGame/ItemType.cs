8b9329d70762fb0bd21ebd450ed15841709dfd03
kiss-mate <kiss.mate.deik@gmail.com>
2021-11-03 20:43:08 +0100

    created ItemType enum

---
+++ LabyrinthGame/ItemType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabyrinthGame
+{
+    enum ItemType
+    {
+        WALL,
+        FIRE,
+        APPLE
+    }
+}
