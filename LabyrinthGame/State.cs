d2aa62828d0258e0e5e76a3444217400ddd24130
kiss-mate <kiss.mate.deik@gmail.com>
2021-11-05 09:35:59 +0100

    State enum created

---
+++ LabyrinthGame/State.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabyrinthGame
+{
+    enum State
+    {
+        PLAYER_WON, PLAYER_LOST, PLAYER_INGAME
+    }
+}
